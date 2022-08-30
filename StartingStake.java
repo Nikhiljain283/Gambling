5f1aeead79f9d8e7491a20ca69192f70fbce09ed
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-08-30 12:57:16 +0530

    Added : starting stake

---
+++ StartingStake.java
@@ -0,0 +1,11 @@
+package com.gambling;
+
+public class StartingStake {
+
+	public static void main(String[] args) {
+		int stake = 100;
+		int bet = 1;
+		System.out.println("Starting stake : 100");
+	}
+
+}
